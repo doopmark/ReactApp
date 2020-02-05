3910067f09a7d0b431af93971096d2bd038fcb1a
Reymark Magsanay <reymark.magsanay@gmail.com>
2020-02-05 22:25:19 +0800

    Updates

---
+++ precache-manifest.fa02c28d60777938731be395fa2ebb59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bba872d4e0766231c3572a4aadd5ae2b",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "a7175b6fd29f6952d797",
+    "url": "/ReactApp/static/css/main.f7a0f023.chunk.css"
+  },
+  {
+    "revision": "c9d2c52f48cdccb5f726",
+    "url": "/ReactApp/static/js/2.4f0bc389.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/ReactApp/static/js/2.4f0bc389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7175b6fd29f6952d797",
+    "url": "/ReactApp/static/js/main.b8aca89c.chunk.js"
+  },
+  {
+    "revision": "a63c37d63ff5a420d742",
+    "url": "/ReactApp/static/js/runtime-main.b3beb9ab.js"
+  }
+]);
